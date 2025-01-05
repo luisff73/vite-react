d834d5287b1185b31936286eff4aab8de01ea776
luisff73 <jvrluis@hotmail.com>
2025-01-05 13:43:06 +0100

    modificado eslin.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,22 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    plugins: {
+      react: pluginReact,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off", // Si usas React 17 o superior, no es necesario importar React
+    },
+  },
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
