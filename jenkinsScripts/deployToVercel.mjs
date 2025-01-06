00710d477af6b3588c946bcec2d6db5ac109a5e3
luisff73 <jvrluis@hotmail.com>
2025-01-06 10:35:10 +0100

    token

--- jenkinsScripts/deployToVercel.mjs
+++ jenkinsScripts/deployToVercel.mjs
@@ -3,11 +3,8 @@ import { execSync } from 'child_process';
 const vercelToken = process.env.VERCEL_TOKEN;
 
 try {
-    // Configura el token de Vercel
-    execSync(`vercel login --token ${vercelToken}`, { stdio: 'inherit' });
-
-    // Ejecuta el comando para desplegar en Vercel con confirmación automática
-    execSync('vercel --prod --yes', { stdio: 'inherit' });
+    // Ejecuta el comando para desplegar en Vercel con confirmación automática y usando el token
+    execSync(`vercel --prod --yes --token ${vercelToken}`, { stdio: 'inherit' });
     console.log('Despliegue a Vercel completado correctamente.');
 } catch (error) {
     console.error('Error durante el despliegue a Vercel:', error);
