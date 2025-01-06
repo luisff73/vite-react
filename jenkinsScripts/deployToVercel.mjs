deae5e5699307ff21b65c4007517049b20db04d0
luisff73 <jvrluis@hotmail.com>
2025-01-06 10:41:02 +0100

    nombre proyecto

--- jenkinsScripts/deployToVercel.mjs
+++ jenkinsScripts/deployToVercel.mjs
@@ -1,10 +1,10 @@
 import { execSync } from 'child_process';
 
 const vercelToken = process.env.VERCEL_TOKEN;
-
+const projectName = 'vite-react'; // Nombre del proyecto
 try {
     // Ejecuta el comando para desplegar en Vercel con confirmación automática y usando el token
-    execSync(`vercel --prod --yes --token ${vercelToken}`, { stdio: 'inherit' });
+    execSync(`vercel --prod --yes --token ${vercelToken} --name ${projectName}`, { stdio: 'inherit' });
     console.log('Despliegue a Vercel completado correctamente.');
 } catch (error) {
     console.error('Error durante el despliegue a Vercel:', error);
