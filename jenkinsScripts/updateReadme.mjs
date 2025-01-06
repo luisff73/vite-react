895692cacb40f7122d71432a957797db98ff83ab
luisff73 <jvrluis@hotmail.com>
2025-01-06 20:39:35 +0100

    test3

--- jenkinsScripts/updateReadme.mjs
+++ jenkinsScripts/updateReadme.mjs
@@ -7,9 +7,15 @@ const badgeFailure = '![Failure](https://img.shields.io/badge/test-failure-red)'
 
 const testResult = process.argv[2]; // 'success' or 'failure'
 
-fs.readFile(readmePath, 'utf8', (err, data) => {
+console.log('Test result passed to script:', testResult);
 
-  //console.log('Current README.md content:', data);
+fs.readFile(readmePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading README.md:', err);
+    process.exit(1);
+  }
+
+  console.log('Current README.md content:', data);
 
   const resultText = 'RESULTADO DE LOS ULTIMOS TESTS';
   const badge = testResult === 'success' ? badgeSuccess : badgeFailure;
@@ -17,7 +23,7 @@ fs.readFile(readmePath, 'utf8', (err, data) => {
     ? data.replace(new RegExp(`${resultText}.*`, 'g'), `${resultText}\n${badge}`)
     : `${data}\n\n${resultText}\n${badge}`;
 
-  //console.log('New README.md content:', newData);
+  console.log('New README.md content:', newData);
 
   fs.writeFile(readmePath, newData, 'utf8', (err) => {
     if (err) {
