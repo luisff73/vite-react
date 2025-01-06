e223c8a7941b172e058547adfbd88012fbdb9af3
luisff73 <jvrluis@hotmail.com>
2025-01-06 10:10:51 +0100

    deploy

--- jenkinsScripts/deployToVercel.mjs
+++ jenkinsScripts/deployToVercel.mjs
@@ -1,9 +1,10 @@
 import { execSync } from 'child_process';
 
 try {
-    // Ejecuta el comando para desplegar en Vercel
-    execSync('vercel --prod', { stdio: 'inherit' });
-    console.log('Despliege a Vercel completadro correctamente.');
+    // Ejecuta el comando para desplegar en Vercel con confirmación automática
+    execSync('vercel --prod --yes', { stdio: 'inherit' });
+    console.log('Despliegue a Vercel completado correctamente.');
 } catch (error) {
-    console.error('Error durante el despliege a Vercel:', error);
+    console.error('Error durante el despliegue a Vercel:', error);
+    process.exit(1);
 }
